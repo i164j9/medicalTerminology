cbbe184bd1639e2e5f881215989d65d312c727ed
Tom Miller <audiotec69@gmail.com>
2023-07-01 13:17:26 -0500

    formatted json files and changed script to format

--- termEntry.py
+++ termEntry.py
@@ -11,28 +11,28 @@ while user_entry != 'quit':
 
     if user_entry[-1] == "-":
         #pre.write("{\"prefix\":\"user_entry\",\"meaning\":\"definition\"},")
-        line_pre ="{\"prefix\":\""
-        line_mid="\",\"meaning\":\""
-        line_post =  "\"},"
+        line_pre ="\n\t\t{\n\t\t\t\"prefix\":\""
+        line_mid="\",\n\t\t\t\"meaning\":\""
+        line_post =  "\"\n\t\t},"
         line = line_pre + user_entry + line_mid + definition + line_post
         print("\n\t"+line)
         pre.write("\n\t"+line)
         pre.flush()
     elif user_entry[-2]+user_entry[-1] == '/o' or user_entry[-2]+user_entry[-1] == '/i' or user_entry[-2]+user_entry[-1] == '/e':
-        line_pre ="{\"root\":\""
-        line_mid="\",\"meaning\":\""
-        line_post =  "\"},"
+        line_pre = "\n\t\t{\n\t\t\t\"root\":\""
+        line_mid = "\",\n\t\t\t\"meaning\":\""
+        line_post = "\"\n\t\t},"
         line = line_pre + user_entry + line_mid + definition + line_post
         print("\n\t"+line)
         rot.write("\n\t"+line)
         rot.flush()
     elif user_entry[0] == '-':
-        line_pre ="{\"suffix\":\""
-        line_mid="\",\"meaning\":\""
-        line_post =  "\"},"
+        line_pre = "\n\t\t{\n\t\t\t\"suffix\":\""
+        line_mid = "\",\n\t\t\t\"meaning\":\""
+        line_post = "\"\n\t\t},"
         line = line_pre + user_entry + line_mid + definition + line_post
-        print("\n\t"+line)
-        suf.write("\n\t"+line)
+        print(line)
+        suf.write(line)
         suf.flush()
     else:
         print("re-enter your term and definition")
